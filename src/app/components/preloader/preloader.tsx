1c0d15ba33d1abb7a86ffd3d2fdbab3b115374b2
Dmytro Sheremetiev <dmytriy.sheremetiev@gmail.com>
2024-12-04 11:59:02 +0200

    chore

--- src/app/components/preloader/preloader.tsx
+++ src/app/components/preloader/preloader.tsx
@@ -1,11 +1,36 @@
 import Lottie from "lottie-react";
-import groovyWalkAnimation from "@/app/assets/lottie/tree.json";
+import animation from "@/app/assets/lottie/sunflower-sign.json";
+import { useEffect, useState } from "react";
 
 export default function Preloader() {
+  const [isVisible, setIsVisible] = useState(true);
+  const [isFading, setIsFading] = useState(false);
+
+  useEffect(() => {
+    document.body.style.overflow = "hidden"; // Включаем скролл
+
+    const timer = setTimeout(() => {
+      document.body.style.overflow = ""; // Включаем скролл
+
+      setIsFading(true);
+      setTimeout(() => setIsVisible(false), 500);
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!isVisible) {
+    return null;
+  }
+
   return (
-    <div className="bg-mainPink fixed top-0 left-0 right-0 bottom-0 z-[1000] flex items-center justify-center">
-      <div className="w-[300px] h-[300px]">
-        <Lottie animationData={groovyWalkAnimation} loop={true} />
+    <div
+      className={`bg-white fixed top-0 left-0 right-0 bottom-0 z-[1000] flex items-center justify-center transition-opacity duration-500 ${
+        isFading ? "opacity-0" : "opacity-100"
+      }`}
+    >
+      <div className="w-full h-full flex items-center justify-center scale-105">
+        <Lottie animationData={animation} loop={true} />
       </div>
     </div>
   );
--- src/app/providers/AppProvider.tsx
+++ src/app/providers/AppProvider.tsx
@@ -1,8 +1,22 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { TranslationsProvider } from "./TranslationProvider";
 
 export default function AppProvider({ children }: { children: ReactNode }) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, 700);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!isVisible) {
+    return null;
+  }
+
   return <TranslationsProvider>{children}</TranslationsProvider>;
 }
