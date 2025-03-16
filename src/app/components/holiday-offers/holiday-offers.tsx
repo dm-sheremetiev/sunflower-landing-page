4f475eb850e248640363653bb6960b92f95099ee
Dmytro Sheremetiev <dmytriy.sheremetiev@gmail.com>
2025-03-16 21:58:51 +0200

    chore

--- src/app/components/holiday-offers/holiday-offers.tsx
+++ src/app/components/holiday-offers/holiday-offers.tsx
@@ -29,7 +29,7 @@ export default function HolidayOffers() {
       <Preloader />
 
       <AppProvider>
-        <Header isExternal />
+        <Header isExternal isHoliday={false} />
 
         <HolidayBanner />
 
