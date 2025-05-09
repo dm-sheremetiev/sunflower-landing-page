b0baf768b8642416677e149e7dd95255233b8fa2
Dmytro Sheremetiev <dmytriy.sheremetiev@gmail.com>
2025-05-09 14:12:52 +0300

    chore: implemented mother's day namings

--- src/app/components/discount-banner/discount-banner.tsx
+++ src/app/components/discount-banner/discount-banner.tsx
@@ -7,7 +7,11 @@ import { useAnimation, motion } from "framer-motion";
 import CopyIcon from "@/app/assets/img/svg/copy-icon.svg";
 import toast, { Toaster } from "react-hot-toast";
 
-export default function DiscountBanner() {
+interface Props {
+  isHoliday?: boolean;
+}
+
+export default function DiscountBanner({ isHoliday }: Props) {
   const { t } = useTranslation();
 
   const [refDisc, inView] = useInView({
@@ -49,7 +53,11 @@ export default function DiscountBanner() {
     >
       <div>
         <Trans
-          i18nKey="discount.discount-percents"
+          i18nKey={
+            isHoliday
+              ? "discount.discount-heading-mothers-day"
+              : "discount.discount-percents"
+          }
           components={{
             customComp: (
               <span className="text-mainRed text-[25px] sm:text-[35px] md:text-[45px] font-semibold italic text-center" />
@@ -59,13 +67,21 @@ export default function DiscountBanner() {
             ),
           }}
         >
-          {t("discount.discount-percents")}
+          {t(
+            isHoliday
+              ? "discount.discount-heading-mothers-day"
+              : "discount.discount-percents"
+          )}
         </Trans>
       </div>
 
       <div className="mt-[25px] md:mt-[35px] flex flex-col gap-3 md:gap-5 items-center">
         <Trans
-          i18nKey="discount.order-in-instagram"
+          i18nKey={
+            isHoliday
+              ? "discount.order-in-instagram-mothers-day"
+              : "discount.order-in-instagram"
+          }
           components={{
             customComp: <span className="text-black italic font-semibold" />,
             h2: (
@@ -73,32 +89,42 @@ export default function DiscountBanner() {
             ),
           }}
         >
-          {t("discount.order-in-instagram")}
+          {t(
+            isHoliday
+              ? "discount.order-in-instagram-mothers-day"
+              : "discount.order-in-instagram"
+          )}
         </Trans>
 
-        <div className="flex items-center gap-[10px] md:gap-[15px]">
-          <h2
-            className="text-[31px] sm:text-[35px] md:text-[50px] font-semibold text-center text-mainRed"
-            onSelect={() => sendGAEvent("event", "discount_select")}
-          >
-            SNFLWR
-          </h2>
+        {!isHoliday ? (
+          <>
+            <div className="flex items-center gap-[10px] md:gap-[15px]">
+              <h2
+                className="text-[31px] sm:text-[35px] md:text-[50px] font-semibold text-center text-mainRed"
+                onSelect={() => sendGAEvent("event", "discount_select")}
+              >
+                SNFLWR
+              </h2>
 
-          <button
-            className="w-[25px] h-[25px] md:w-[35px] md:h-[35px]"
-            onClick={() => {
-              navigator.clipboard.writeText("SNFLWR");
-              toast.success(t("discount.discount-copied-successfully"), {
-                position: "top-center",
-              });
-            }}
-          >
-            <CopyIcon />
-          </button>
-        </div>
-        {/* <div className="w-full flex justify-center">
-          <p className="self-start text-left">{t("discount.discount-off")}</p>
-        </div> */}
+              <button
+                className="w-[25px] h-[25px] md:w-[35px] md:h-[35px]"
+                onClick={() => {
+                  navigator.clipboard.writeText("SNFLWR");
+                  toast.success(t("discount.discount-copied-successfully"), {
+                    position: "top-center",
+                  });
+                }}
+              >
+                <CopyIcon />
+              </button>
+            </div>
+            <div className="w-full flex justify-center">
+              <p className="self-start text-left">
+                {t("discount.discount-off")}
+              </p>
+            </div>
+          </>
+        ) : null}
 
         <Toaster />
       </div>
